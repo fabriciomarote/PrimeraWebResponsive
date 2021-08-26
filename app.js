66e39bc6f6177fca7c4913dbd731bb4b0b4d6b8e
fabriciomarote <fabrii.cai93@gmail.com>
2021-08-25 21:12:08 -0300

    inicio pagina web

---
+++ app.js
@@ -0,0 +1,25 @@
+const boton = document.querySelector('#boton');
+
+const configUser = window.matchMedia('(prefers-color-scheme: dark)');
+
+const localConfig = localStorage.getItem('tema');
+if ( localConfig === 'dark') {
+    document.body.classList.toggle('dark-theme');  
+} else {
+    document.body.classList.toggle('light-theme');
+}
+
+boton.addEventListener('click', ()=> {
+    //console.log(configUser.matches)
+    let colorTema;
+    if ( configUser.matches ) {
+        //entramos con modo oscuro
+        document.body.classList.toggle('light-theme');
+        colorTema = document.body.classList.contains('light-theme') ? 'light' : 'dark';
+
+    } else {
+        document.body.classList.toggle('dark-theme');
+        colorTema = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+    }
+    localStorage.setItem('tema', colorTema);
+});
